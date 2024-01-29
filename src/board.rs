d8a23528642f299c0d5c0f14e1ac1943d4460664
Ruan Comelli <ruancomelli@gmail.com>
2024-01-29 09:36:00 -0300

    refactor: implement `Drawable` trait for drawers for players, cells and boards

--- src/board.rs
+++ src/board.rs
@@ -1,6 +1,7 @@
-use crate::players::Player;
+use crate::players::{Player, PlayerDrawer};
+use ggez::context::Has;
 use ggez::glam::Vec2;
-use ggez::graphics::Color;
+use ggez::graphics::{Canvas, Color, DrawMode, DrawParam, Drawable, GraphicsContext, Mesh, Rect};
 
 pub const BOARD_SIZE: f32 = 300.0;
 pub const CELL_SIZE: f32 = BOARD_SIZE / 3.0;
@@ -27,6 +28,15 @@ impl Cell {
         self.player.is_none()
     }
 
+    pub fn bounding_box(&self) -> Rect {
+        Rect::new(
+            self.col as f32 * CELL_SIZE,
+            self.row as f32 * CELL_SIZE,
+            CELL_SIZE,
+            CELL_SIZE,
+        )
+    }
+
     pub fn top_left(&self) -> Vec2 {
         Vec2 {
             x: self.col as f32 * CELL_SIZE,
@@ -43,6 +53,61 @@ impl Cell {
             None => Color::WHITE,
             Some(player) => player.color(),
         }
+    }
+}
+
+struct CellDrawer<'a, 'b> {
+    pub ctx: &'a GraphicsContext,
+    pub cell: &'b Cell,
+    pub is_hovered: bool,
+    pub is_winner: bool,
+    pub line_width: f32,
+}
+
+impl Drawable for CellDrawer<'_, '_> {
+    fn draw(&self, canvas: &mut Canvas, param: impl Into<DrawParam>) {
+        let param = param.into();
+
+        let color = match self.cell.player {
+            None => Color::WHITE,
+            Some(player) => {
+                if self.is_winner {
+                    Color::YELLOW
+                } else {
+                    player.color()
+                }
+            }
+        };
+
+        if let Some(player) = self.cell.player {
+            let player_drawer = PlayerDrawer {
+                ctx: self.ctx,
+                player,
+                bounding_box: self.cell.bounding_box(),
+                color,
+                line_width: self.line_width,
+            };
+
+            player_drawer.draw(canvas, param);
+        }
+
+        let cell_mesh = Mesh::new_rectangle(
+            self.ctx,
+            if self.is_hovered {
+                DrawMode::stroke(self.line_width)
+            } else {
+                DrawMode::fill()
+            },
+            self.cell.bounding_box(),
+            set_color_transparency(self.cell.color(), 255.0 / 4.0),
+        )
+        .expect("Failed to create cell mesh");
+
+        canvas.draw(&cell_mesh, param);
+    }
+
+    fn dimensions(&self, _gfx: &impl Has<GraphicsContext>) -> Option<Rect> {
+        Some(self.cell.bounding_box())
     }
 }
 
@@ -110,4 +175,90 @@ impl Board {
             })
             .collect()
     }
+}
+
+pub struct BoardDrawer<'a, 'b> {
+    pub ctx: &'a GraphicsContext,
+    pub board: &'b Board,
+    pub mouse_on_cell: Option<Cell>,
+    pub winner_cells: Vec<Cell>,
+    pub line_width: f32,
+}
+
+impl Drawable for BoardDrawer<'_, '_> {
+    fn draw(&self, canvas: &mut Canvas, param: impl Into<DrawParam>) {
+        let param = param.into();
+
+        let board_mesh = Mesh::new_rectangle(
+            self.ctx,
+            DrawMode::fill(),
+            Rect::new(0.0, 0.0, BOARD_SIZE, BOARD_SIZE),
+            Color::BLACK,
+        )
+        .expect("Failed to create board mesh");
+
+        canvas.draw(&board_mesh, param);
+
+        for cell in self.board.cells.iter().flatten() {
+            let cell_drawer = CellDrawer {
+                ctx: self.ctx,
+                cell,
+                is_hovered: self.mouse_on_cell == Some(*cell),
+                is_winner: self.winner_cells.contains(cell),
+                line_width: self.line_width,
+            };
+
+            cell_drawer.draw(canvas, param);
+        }
+
+        for i in 1..3 {
+            let vertical_rule = Mesh::new_line(
+                self.ctx,
+                &[
+                    Vec2 {
+                        x: i as f32 * CELL_SIZE,
+                        y: 0.0,
+                    },
+                    Vec2 {
+                        x: i as f32 * CELL_SIZE,
+                        y: BOARD_SIZE,
+                    },
+                ],
+                2.0,
+                Color::WHITE,
+            )
+            .expect("Failed to create vertical rule");
+
+            canvas.draw(&vertical_rule, Vec2::ZERO);
+        }
+
+        for i in 1..4 {
+            let horizontal_rule = Mesh::new_line(
+                self.ctx,
+                &[
+                    Vec2 {
+                        x: 0.0,
+                        y: i as f32 * CELL_SIZE,
+                    },
+                    Vec2 {
+                        x: BOARD_SIZE,
+                        y: i as f32 * CELL_SIZE,
+                    },
+                ],
+                2.0,
+                Color::WHITE,
+            )
+            .expect("Failed to create horizontal rule");
+
+            canvas.draw(&horizontal_rule, Vec2::ZERO);
+        }
+    }
+
+    fn dimensions(&self, _gfx: &impl Has<GraphicsContext>) -> Option<Rect> {
+        Some(Rect::new(0.0, 0.0, BOARD_SIZE, BOARD_SIZE))
+    }
+}
+
+fn set_color_transparency(color: Color, a: f32) -> Color {
+    Color::new(color.r, color.g, color.b, a)
 }
--- src/players.rs
+++ src/players.rs
@@ -1,6 +1,7 @@
-use ggez::graphics::Color;
+use ggez::context::Has;
+use ggez::graphics::{Canvas, Color, DrawMode, DrawParam, Drawable, GraphicsContext, Mesh, Rect};
+use ggez::mint::Point2;
 use std::fmt::Display;
-
 #[derive(PartialEq, Clone, Copy)]
 pub enum Player {
     X,
@@ -8,17 +9,17 @@ pub enum Player {
 }
 
 impl Player {
-    pub fn next(&self) -> Player {
+    pub fn next(&self) -> Self {
         match self {
-            Player::X => Player::O,
-            Player::O => Player::X,
+            Self::X => Self::O,
+            Self::O => Self::X,
         }
     }
 
     pub fn color(&self) -> Color {
         match self {
-            Player::X => Color::RED,
-            Player::O => Color::BLUE,
+            Self::X => Color::RED,
+            Self::O => Color::BLUE,
         }
     }
 }
@@ -26,8 +27,121 @@ impl Player {
 impl Display for Player {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
-            Player::X => write!(f, "X"),
-            Player::O => write!(f, "O"),
+            Self::X => write!(f, "X"),
+            Self::O => write!(f, "O"),
         }
+    }
+}
+
+pub struct PlayerDrawer<'a> {
+    pub ctx: &'a GraphicsContext,
+    pub player: Player,
+    pub bounding_box: Rect,
+    pub color: Color,
+    pub line_width: f32,
+}
+
+impl<'a> PlayerDrawer<'a> {
+    pub fn left(&self) -> f32 {
+        self.bounding_box.left()
+    }
+
+    pub fn right(&self) -> f32 {
+        self.bounding_box.right()
+    }
+
+    pub fn width(&self) -> f32 {
+        self.bounding_box.w
+    }
+
+    pub fn top(&self) -> f32 {
+        self.bounding_box.top()
+    }
+
+    pub fn bottom(&self) -> f32 {
+        self.bounding_box.bottom()
+    }
+
+    pub fn height(&self) -> f32 {
+        self.bounding_box.h
+    }
+
+    pub fn center(&self) -> Point2<f32> {
+        self.bounding_box.center()
+    }
+
+    pub fn top_left(&self) -> Point2<f32> {
+        Point2 {
+            x: self.left(),
+            y: self.top(),
+        }
+    }
+
+    pub fn top_right(&self) -> Point2<f32> {
+        Point2 {
+            x: self.right(),
+            y: self.top(),
+        }
+    }
+
+    pub fn bottom_left(&self) -> Point2<f32> {
+        Point2 {
+            x: self.left(),
+            y: self.bottom(),
+        }
+    }
+
+    pub fn bottom_right(&self) -> Point2<f32> {
+        Point2 {
+            x: self.right(),
+            y: self.bottom(),
+        }
+    }
+}
+
+impl<'a> Drawable for PlayerDrawer<'a> {
+    fn draw(&self, canvas: &mut Canvas, param: impl Into<DrawParam>) {
+        let param = param.into();
+
+        let line_width = self.line_width;
+
+        match self.player {
+            Player::X => {
+                let line1 = Mesh::new_line(
+                    self.ctx,
+                    &[self.top_left(), self.bottom_right()],
+                    line_width,
+                    self.color,
+                )
+                .expect("Failed to create line '\\' for X");
+                let line2 = Mesh::new_line(
+                    self.ctx,
+                    &[self.top_right(), self.bottom_left()],
+                    line_width,
+                    self.color,
+                )
+                .expect("Failed to create line '/' for X");
+
+                canvas.draw(&line1, param);
+                canvas.draw(&line2, param);
+            }
+            Player::O => {
+                let circle_border = Mesh::new_ellipse(
+                    self.ctx,
+                    DrawMode::stroke(line_width),
+                    self.center(),
+                    self.width() / 2.0,
+                    self.height() / 2.0,
+                    1.0,
+                    self.color,
+                )
+                .expect("Failed to create circle for O");
+                canvas.draw(&circle_border, param);
+            }
+        }
+    }
+
+    fn dimensions(&self, _gfx: &impl Has<GraphicsContext>) -> Option<Rect> {
+        Some(self.bounding_box)
     }
 }
